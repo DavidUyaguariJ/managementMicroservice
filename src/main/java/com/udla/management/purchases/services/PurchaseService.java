d12d61cc92e433379418a462827e0536bd3c4904
PUSSSO <paul.robalino@udla.edu.ec>
2025-01-26 23:37:51 -0500

    refactor:Inventory logic is eliminated

--- src/main/java/com/udla/management/purchases/services/PurchaseService.java
+++ src/main/java/com/udla/management/purchases/services/PurchaseService.java
@@ -1,7 +1,5 @@
 package com.udla.management.purchases.services;
 
-import com.udla.management.inventory.models.InventoryModel;
-import com.udla.management.inventory.services.InventoryService;
 import com.udla.management.purchases.models.PurchaseModel;
 import com.udla.management.purchases.repositories.PurchaseRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +12,6 @@ import java.util.List;
 public class PurchaseService implements IPurchaseService {
 
     private final PurchaseRepository purchaseRepository;
-    private final InventoryService inventoryService;
 
     @Override
     public List<PurchaseModel> getPurchases() {
@@ -23,35 +20,14 @@ public class PurchaseService implements IPurchaseService {
 
     @Override
     public boolean savePurchase(PurchaseModel purchase) {
-        boolean saveInventory = false;
-        InventoryModel inventoryModel = inventoryService.findInventoryByProduct(purchase.getIdeProduct());
-        if (inventoryModel == null) {
-            inventoryModel = new InventoryModel();
-            inventoryModel.setIdeProduct(purchase.getIdeProduct());
-            inventoryModel.setQuantity(purchase.getQuantity());
-            saveInventory = inventoryService.saveToInventory(inventoryModel);
-        } else {
-            saveInventory = inventoryService.editInventory(inventoryModel);
-        }
-        if (saveInventory) {
             purchaseRepository.save(purchase);
             return true;
-        } else {
-            return false;
-        }
     }
 
     @Override
     public boolean deletePurchase(Long id) {
         if (purchaseRepository.existsById(id)) {
-            PurchaseModel purchase = purchaseRepository.findById(id).orElse(null);
-            InventoryModel inventoryModel = inventoryService.findInventoryByProduct(purchase.getIdeProduct());
-            if (inventoryModel.getQuantity() - purchase.getQuantity() < 0) {
-                return false;
-            }
-            inventoryModel.setQuantity(inventoryModel.getQuantity() - purchase.getQuantity());
             purchaseRepository.deleteById(id);
-            inventoryService.deleteFromInventory(id);
             return true;
         }
         return false;
@@ -64,28 +40,14 @@ public class PurchaseService implements IPurchaseService {
 
     @Override
     public boolean editPurchase(PurchaseModel purchase) {
-        InventoryModel inventoryModel = inventoryService.findInventoryByProduct(purchase.getIdeProduct());
-        if (inventoryModel == null) {
-            return false;
-        }
         int quantity=0;
-        if(inventoryModel.getQuantity()>purchase.getQuantity()){
-            quantity=inventoryModel.getQuantity() - purchase.getQuantity();
-        }else if(inventoryModel.getQuantity()<purchase.getQuantity()){
-            quantity=purchase.getQuantity()-inventoryModel.getQuantity();
-        }
         if (quantity < 0) {
             return false;
         }
-        boolean saveInventory = inventoryService.editInventory(inventoryModel);
-        if (saveInventory) {
-            if (purchaseRepository.existsById(purchase.getIdePurchase())) {
-                purchaseRepository.save(purchase);
-                return true;
-            }
-        } else {
-            return false;
-        }
+         if (purchaseRepository.existsById(purchase.getIdePurchase())) {
+            purchaseRepository.save(purchase);
+            return true;
+           }
         return false;
     }
 }
