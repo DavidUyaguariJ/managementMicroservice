7a0276a7283afa40217e08c7901645a7ca1123bc
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2025-12-02 08:40:47 -0500

    feat: change dependencies versions

--- src/main/java/com/udla/management/TicketsApplication.java
+++ src/main/java/com/udla/management/TicketsApplication.java
@@ -5,21 +5,20 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 @SpringBootApplication
 public class TicketsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration()
-				.setSkipNullEnabled(true)
-				.setMatchingStrategy(MatchingStrategies.STRICT);
-		return modelMapper;
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 
 }
