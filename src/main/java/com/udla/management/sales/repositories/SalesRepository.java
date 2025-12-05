73b0f4af6791c5b1b44fef7d15282ce93b46adc0
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2025-12-04 23:47:44 -0500

    fix: change checkstyle

--- src/main/java/com/udla/management/sales/repositories/SalesRepository.java
+++ src/main/java/com/udla/management/sales/repositories/SalesRepository.java
@@ -4,9 +4,17 @@ import com.udla.management.sales.models.SaleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for managing SaleModel database operations.
+ */
 @Repository
 public interface SalesRepository extends JpaRepository<SaleModel, Long> {
 
+    /**
+     * Checks whether a sale exists with the given description.
+     *
+     * @param description sale description
+     * @return true if exists, false otherwise
+     */
     boolean existsByDescription(String description);
-
 }
